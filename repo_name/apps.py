21ebdc0515e0ed9057d88faa1f889d99a4640700
YuhaoT <heyuhao97@gamil.com>
2023-09-11 16:15:15 -0400

    plugin manual setup

--- repo_name/apps.py
+++ repo_name/apps.py
@@ -33,29 +33,7 @@ class MyAppConfig(AppConfig):
                 PluginURLs.RELATIVE_PATH: 'api.urls',
             }
         },
-
-        # Configuration setting for Plugin Settings for this app.
-        PluginSettings.CONFIG: {
-
-            # Configure the Plugin Settings for each Project Type, as needed. The full list of setting types for edx-platform is
-            # here:
-            # https://github.com/openedx/edx-platform/blob/2dc79bcab42dafed2c122eb808cdd5604327c890/openedx/core/djangoapps/plugins/constants.py#L25 .
-            # Other IDAs may use different values.
-            'lms.djangoapp': {
-
-                # Configure each settings, as needed.
-                'production': {
-
-                    # The python path (relative to this app) to the settings module for the relevant Project Type and Settings Type.
-                    # Optional; Defaults to 'settings'.
-                    PluginSettings.RELATIVE_PATH: 'settings.production',
-                },
-                'common': {
-                    PluginSettings.RELATIVE_PATH: 'settings.common',
-                },
-            }
-        },
-
+        
         # Configuration setting for Plugin Signals for this app.
         PluginSignals.CONFIG: {
 
