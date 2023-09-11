59c348284400ab6562fa20fb92029cbe1759a66f
YuhaoT <heyuhao97@gamil.com>
2023-09-11 15:23:52 -0400

    init commit

---
+++ docs/index.rst
@@ -0,0 +1,34 @@
+.. repo_name documentation top level file, created by
+   sphinx-quickstart on Mon Sep 11 14:44:24 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+repo_name
+=========
+
+One-line description for README and other doc files.
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   getting_started
+   quickstarts/index
+   concepts/index
+   how-tos/index
+   testing
+   internationalization
+   modules
+   changelog
+   decisions
+   references/index
+
+
+Indices and tables
+##################
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ repo_name/apps.py
@@ -0,0 +1,31 @@
+import logging
+from django.apps import AppConfig
+from edx_django_utils.plugins import PluginSettings, PluginURLs
+from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
+
+log = logging.getLogger(__name__)
+
+class MyAppConfig(AppConfig):
+    name = "my_app"
+    label = "my_app"
+    verbose_name = "My Open edX Plugin"
+
+    # See: https://edx.readthedocs.io/projects/edx-django-utils/en/latest/edx_django_utils.plugins.html
+    plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: name,
+                PluginURLs.REGEX: "^my-app/",
+                PluginURLs.RELATIVE_PATH: "urls",
+            }
+        },
+        PluginSettings.CONFIG: {
+            ProjectType.LMS: {
+                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: "settings.production"},
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: "settings.common"},
+            }
+        },
+    }
+
+    def ready(self):
+        log.debug("{label} is ready.".format(label=self.label))
